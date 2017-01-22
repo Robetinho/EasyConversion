c7b7059000767c3b88f570d75a5a3d0b7166169e
Robetinho <robertinho_66@hotmail.com>
2017-01-22 16:15:29 +0700

    Creation
    uploading the project

---
+++ ConversionTests/UnitTests.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Conversions;
+
+namespace ConversionTests
+{
+    [TestClass]
+    public class UnitTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual(1.TryCastAs<string>(), "1");
+            Assert.AreEqual(true, 1.CanCastAs<string>());
+
+            Assert.AreEqual(1.TryCastAs<int>(), 1);
+            Assert.AreEqual(true, 1.CanCastAs<int>());
+
+            Assert.AreEqual("123466".TryCastAs<int>(), 123466);
+            Assert.AreEqual(true, "123466".CanCastAs<int>());
+
+            Assert.AreEqual("1.1".TryCastAs<double>(), 1.1);
+
+            Assert.AreEqual("234wgser".TryCastAs<int>(), 0);
+
+            Assert.AreEqual(1.TryCastAs<double>(), 1.0);
+
+            Assert.AreEqual("234wgser".TryCastAs<int?>(), null);
+            Assert.AreEqual(false, "234wgser".CanCastAs<int>());
+
+            int? a = 1;
+            Assert.AreEqual(a, "1".TryCastAs<int?>());
+            a = null;
+            Assert.AreEqual(null, "1dfgf".TryCastAs<int?>());
+            a = null;
+            Assert.AreEqual(1, "1dfgf".TryCastAs<int?>(1));
+
+            Assert.AreEqual(null, new NullReferenceException().TryCastAs<string>());
+            Assert.AreEqual(false, new NullReferenceException().CanCastAs<string>());
+
+            Assert.AreEqual("hello", new NullReferenceException().TryCastAs<string>("hello"));
+
+            Assert.AreEqual("1", 1.TryCastAs<string>("hello"));
+
+
+
+        }
+    }
+}
---
+++ Conversions/Conversions.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Conversions
+{
+    public static class Conversions
+    {
+        
+        public static T CastAs<T>(this object Value)
+        {
+            return (T)Convert.ChangeType(Value, Nullable.GetUnderlyingType(typeof(T)) ?? typeof(T));
+        }
+         
+        public static T TryCastAs<T>(this object Value, T Default = default(T))  
+        { 
+            T response;
+              
+            try
+            {
+                response = Value.CastAs<T>();
+            }
+            catch
+            {
+                response = Default;
+            }
+             
+            return response; 
+        }
+
+        public static bool CanCastAs<T>(this object Value)
+        {
+            bool response = true;
+            try
+            {
+                Value.CastAs<T>();
+            }
+            catch
+            {
+                response = false;
+            }
+
+            return response;
+        }
+    }
+}
